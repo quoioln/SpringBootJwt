9e60737b82d8776e03b15194c54f1582878267cc
Vo Phu Quoi <vpquoi@tma.com.vn>
2020-07-17 17:17:30 +0700

    Initialize project

---
+++ src/main/java/com/vpquoi/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.vpquoi.example.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public ResponseEntity<?> hello() {
+        return ResponseEntity.ok("Hello world!!!!!!!!");
+    }
+}
